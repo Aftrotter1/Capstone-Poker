ef0560a8651c8e878b40b29730e72f07ee34ed87
aftrotter1 <aft0308@gmail.com>
2025-02-18 01:17:20 +0000

    background image works

--- Capstone_Poker_Django/Capstone_Poker_Django/settings.py
+++ Capstone_Poker_Django/Capstone_Poker_Django/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'poker.apps.PokerConfig',
+    'poker'
 ]
 
 MIDDLEWARE = [
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS= (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -127,4 +128,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'poker/bots')
 MEDIA_URL = '/bots/'
 
-STATIC_ROOT= os.path.join(BASE_DIR, 'static')
