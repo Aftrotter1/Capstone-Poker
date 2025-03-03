50c2a3cac897641cbef86942a87d1accdd6a7d6a
nyxslush <nyxslush@gmail.com>
2025-03-03 14:40:00 -0600

    small registration redirection fix

--- Capstone_Poker_Django/poker/views.py
+++ Capstone_Poker_Django/poker/views.py
@@ -145,7 +145,7 @@ class RegisterView(View):
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}')
 
-            return redirect(to='')
+            return redirect(to='/')
 
         return render(request, self.template_name, {'form': form})
     
