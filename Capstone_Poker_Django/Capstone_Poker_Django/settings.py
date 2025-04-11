a257e58df86e41c79a83fe93c2b7db3e0179db58
Aftrotter1 <aft0308@gmail.com>
2025-04-10 19:20:27 -0500

    project dockerized

--- Capstone_Poker_Django/Capstone_Poker_Django/settings.py
+++ Capstone_Poker_Django/Capstone_Poker_Django/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = bool(int(os.getenv('DEBUG',0)))
  
 ALLOWED_HOSTS = ['capstone-poker.appspot.com', 'localhost', '127.0.0.1']
 
